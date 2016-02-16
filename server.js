e7f117a1dfbca5cd008d137c0476ab0eabd070e6
hezag <bielz@msn.com>
2016-02-16 14:39:49 -0200

    support to urls without http, response errors now are reported

--- server.js
+++ server.js
@@ -14,10 +14,17 @@ var express = require('express'),
 var port = 14570;
 var appUserAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36';
 
+function isURL(str) {
+		return /^(https?):\/\/((?:[a-z0-9.-]|%[0-9A-F]{2}){3,})(?::(\d+))?((?:\/(?:[a-z0-9-._~!$&'()*+,;=:@]|%[0-9A-F]{2})*)*)(?:\?((?:[a-z0-9-._~!$&'()*+,;=:\/?@]|%[0-9A-F]{2})*))?(?:#((?:[a-z0-9-._~!$&'()*+,;=:\/?@]|%[0-9A-F]{2})*))?$/i.test(str);
+}
+
 router.get('/get', function(req, res) {
-    var start = new Date();
-    if(typeof req.query.url != "undefined" && req.query.url.length > 6) {
-        var _url = req.query.url;
+    var start = new Date(),
+        _url = req.query.url;
+    if(!_url.startsWith("http")) {
+        _url = "http://" + _url;
+    }
+    if(typeof req.query.url != "undefined" && req.query.url.length > 6 && isURL(_url)) {
         request({
             url: _url,
             encoding: null,
@@ -28,16 +35,16 @@ router.get('/get', function(req, res) {
         function(error, response, body) {
             var _response =
             {
-                contents: body.toString(),
+                contents: (error != null) ? error.toString() : body.toString(),
                 status: {
                     "url": unescape(_url),
-                    "content_type": response.headers['content-type'],
-                    "http_code": response.statusCode,
+                    "content_type": (error != null) ?  '' : response.headers['content-type'],
+                    "http_code": (error != null) ? '0' : response.statusCode,
                     "response_time": (new Date() - start)
                 }
             };
 
-            if (response.headers['content-encoding'] == 'gzip') {
+            if ((error == null) && response.headers['content-encoding'] == 'gzip') {
                 zlib.gunzip(body, function(err, dezipped) {
                     _response.contents = dezipped.toString();
                     res.jsonp(_response);
