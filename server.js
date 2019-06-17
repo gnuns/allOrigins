3a388607cb7103903b878067597af5a66a3fa03b
MichaelGuardian <30236688+MichaelGuardian@users.noreply.github.com>
2019-06-17 19:59:04 +0200

    Added https

--- server.js
+++ server.js
@@ -5,7 +5,18 @@
  */
 
 const app = require('./app.js')
+const https = require('https')
+const fs = require('fs')
+
+var key = fs.readFileSync(__dirname + '/../certs/selfsigned.key')
+var cert = fs.readFileSync(__dirname + '/../certs/selfsigned.crt')
+var options = {
+  key: key,
+  cert: cert
+}
+
+var server = https.createServer(options, app)
 const port = process.env.PORT || 1458
 
 console.log(`Starting allOrigins v${global.AO_VERSION}`)
-app.listen(port, () => console.log('Listening on', port))
+server.listen(port, () => console.log('Listening on', port))
