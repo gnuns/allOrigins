4b3946ad30f893ea135e617efdcde5da6c0ef505
Sourav Kunda <50537259+07souravkunda@users.noreply.github.com>
2020-07-22 10:35:43 +0530

    Update redis.js
    Co-authored-by: gabriel nunes <981105+gnuns@users.noreply.github.com>

--- redis.js
+++ redis.js
@@ -1,5 +1,5 @@
 const redis = require("redis");
-const redisUrl = "redis://127.0.0.1:6379";
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
 const util = require("util");
 client.get = util.promisify(client.get);
