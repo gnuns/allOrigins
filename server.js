5fd984f06bffd540a9ba9a56a45b3e8cf90cd6ce
Red Saint <101050767+red-saint-nft@users.noreply.github.com>
2022-07-26 17:12:45 -0500

    Add https

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-/*!
- * AllOrigins
- * written by Gabriel Nunes <gabriel@multiverso.me>
- * http://github.com/gnuns
- */
-
+const https = require("https");
+ const  fs = require("fs");
 const app = require('./app.js')
-const port = process.env.PORT || 1458
+const options = {
+  key: fs.readFileSync("./api.sdom.com.key"),
+  cert: fs.readFileSync("./api_sdom_com.crt"),
+  ca:   fs.readFileSync('./api_sdom_com.ca-bundle')
+
+};
+
+
+const port = process.env.PORT || 80
 
 console.log(`Starting allOrigins v${global.AO_VERSION}`)
+
 app.listen(port, () => console.log('Listening on', port))
+
+
+https.createServer(options, app).listen(443, ()=>console.log('listening on ', 443));
